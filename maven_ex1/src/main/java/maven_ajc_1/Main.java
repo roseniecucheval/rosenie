e5d514063b60969fd564bf92d5f883571c6db39a
Rosenie Cucheval <roseniecucheval@gmail.com>
2024-04-17 16:46:38 +0200

    Ajout d'un projet Java

---
+++ maven_ex1/src/main/java/maven_ajc_1/Main.java
@@ -0,0 +1,10 @@
+package maven_ajc_1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("------------");
+        System.out.println("Hello world!");
+        System.out.println("------------");
+    }
+
+}
---
+++ maven_ex1/src/test/java/maven_ajc_1/App.java
@@ -0,0 +1,9 @@
+package maven_ajc_1;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("---------");
+        System.out.println("Test : OK");
+        System.out.println("---------");
+    }
+}
