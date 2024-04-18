1908fc3805d502cae96d152efaf75308a04fd516
Rosenie Cucheval <89519284+roseniecucheval@users.noreply.github.com>
2024-04-18 09:18:49 +0000

    Test de conflit

--- Test.java
+++ Test.java
@@ -3,4 +3,9 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Yes");
     }
+
+    public void m1() {
+        String test = "yes";
+        System.out.println("1 méthode avec conflit" + yes);
+    }
 }
