54b2ac513661db9385ccf7e2ea4f837139389ed8
Rosenie Cucheval <89519284+roseniecucheval@users.noreply.github.com>
2024-04-18 09:17:18 +0000

    Nouvelle méthode

--- Test.java
+++ Test.java
@@ -3,4 +3,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Yes");
     }
+
+    public void m1() {
+        System.out.println("1 méthode avec conflit");
+    }
 }
