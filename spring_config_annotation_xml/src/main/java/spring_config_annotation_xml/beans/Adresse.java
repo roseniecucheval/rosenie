d2ef9fec31cb98d5f1cebd165f7dd731ee710ce6
Rosenie Cucheval <89519284+roseniecucheval@users.noreply.github.com>
2024-04-23 09:07:53 +0000

    Ajout du dossier spring_config_annotation_xml (Spring)

---
+++ spring_config_annotation_xml/src/main/java/spring_config_annotation_xml/beans/Adresse.java
@@ -0,0 +1,18 @@
+package spring_config_annotation_xml.beans;
+
+public class Adresse {
+    
+    private String adresse;
+
+    public Adresse() {
+
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+}
---
+++ spring_config_annotation_xml/src/main/java/spring_config_annotation_xml/beans/Personne.java
@@ -0,0 +1,37 @@
+package spring_config_annotation_xml.beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("Olivier")
+public class Personne {
+
+    private String nom = "Olivier";
+    @Autowired
+    private Adresse adresse;
+
+    // Indispensable pour Spring
+    public Personne() {
+
+    }
+
+    public void bonjour() {
+        System.out.println("Je suis une personne.");
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public Adresse getAdresse() {
+        return adresse;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setAdresse(Adresse adresse) {
+        this.adresse = adresse;
+    }
+}
